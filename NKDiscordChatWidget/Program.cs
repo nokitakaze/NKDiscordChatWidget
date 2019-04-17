415d5bb74b4b61e0a31f02bc2ea72cb9bf938c98
Nokita Kaze <admin@kanaria.ru>
2019-04-17 22:22:53 +0300

    Init commit

---
+++ NKDiscordChatWidget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NKDiscordChatWidget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
